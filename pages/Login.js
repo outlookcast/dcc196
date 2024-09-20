e3896bc64a335b0ba02b31bc73e2c420055c854e
Vinicius Sampaio <viniciuscastrosampaio@gmail.com>
2024-09-20 19:59:28 -0300

    Criando pagina de login

---
+++ pages/Login.js
@@ -0,0 +1,84 @@
+import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.font}>E-mail:</Text>
+        <TextInput
+          editable
+          value={email}
+          maxLength={100}
+          onChange={(text) => setEmail(text)}
+          style={styles.input}
+        />
+      </View>
+      <View>
+        <Text style={styles.font}>Senha:</Text>
+        <TextInput
+          editable
+          value={senha}
+          maxLength={100}
+          onChange={(text) => setSenha(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+      <View>
+        <Pressable
+          title="Entrar"
+          color="#3A4D6A"
+          accessibilityLabel="Learn more about this purple button"
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Entrar</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    width: "100%",
+    borderColor: "#3A4D6A",
+    borderWidth: 1,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  font: {
+    fontSize: 30,
+    textAlign: "left",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#3A4D6A",
+    width: "100%",
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  view: {
+    alignItems: "center",
+  },
+});
