5542acd15f479c10733cc940382ffcc0ddb0a574
Matheus Peron <matheusprcorrea@gmail.com>
2024-09-25 02:31:36 -0300

    Estilização Global da barra de navegação e adição de novas telas

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import * as React from 'react'; 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button, TouchableOpacity, Text } from 'react-native';
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
+import NovaAula from "./pages/novaAula";
+import TelaPrincipal from "./pages/TelaPrincipal";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-  <NavigationContainer>
-      <Stack.Navigator initialRouteName='login'>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="login" 
+      screenOptions={{
+        headerTitleAlign: "center",
+        headerStyle: {
+          backgroundColor: "#3A4D6A", 
+          height: 100, 
+        },
+        headerTintColor: "#fff", 
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontSize: 24,
+        },
+        headerRight: () => (
+          <TouchableOpacity>
+            <Text style={{ color: "#fff", marginRight: 10 }}>Info</Text>
+          </TouchableOpacity>
+        ),
+      }}>
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="TelaPrincipal" component={TelaPrincipal} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
+        <Stack.Screen name="Nova Aula" component={NovaAula}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
- 
 }
