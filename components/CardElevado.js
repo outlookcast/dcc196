8511130405da7e9f8e2ce6221967abde3644ea73
Matheus Peron <matheusprcorrea@gmail.com>
2024-09-25 02:34:45 -0300

    Add: layout de Cartão Elevado

---
+++ components/CardElevado.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CardElevado = ({ children, style }) => {
+  return (
+    <View style={[styles.card, style]}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+card: {
+    width: '85%', 
+    height: 650, 
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10, 
+    shadowColor: "#000", 
+    shadowOffset: { width: 0, height: 4 }, 
+    shadowOpacity: 0.3, 
+    shadowRadius: 10, 
+    elevation: 10,
+    },
+});
+
+export default CardElevado;
