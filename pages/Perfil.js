4036f62ef1475aa16cb245438e147e690a6339cb
Matheus Peron <matheusprcorrea@gmail.com>
2024-09-30 13:46:06 -0300

    Adicionando modal e inputs

--- pages/Perfil.js
+++ pages/Perfil.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   StyleSheet,
   View,
   Text,
   Pressable,
   TouchableOpacity,
+  Modal,
+  ScrollView,
+  TextInput,
 } from "react-native";
 import CardElevado from "../components/CardElevado";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { Input } from "react-native-elements";
 
-const Perfil = () => {
+const Perfil = ({navigation}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const fechar = () => {
+    navigation.navigate("Tela Principal", {})
+  };
   return (
-    <View style={styles.container}>
+    <ScrollView>
+<View style={styles.container}>
       <CardElevado style={styles.customCard}>
         <View style={{ flexDirection: "row" }}>
           <MaterialIcons name="account-circle" size={100} color="#3A4D6A" />
@@ -24,19 +34,22 @@ const Perfil = () => {
           <Ionicons name="person" size={30} color="#3A4D6A" />
           <Text style={styles.tituloText}> Nome </Text>
         </View>
+        <Input keyboardType="default"></Input> 
         <View style={styles.titulo}>
           <MaterialIcons name="cake" size={30} color="#3A4D6A" />
           <Text style={styles.tituloText}> Idade </Text>
         </View>
+        <Input keyboardType="numeric"></Input>
         <View style={styles.titulo}>
           <Ionicons name="mail" size={30} color="#3A4D6A" />
           <Text style={styles.tituloText}> E-mail </Text>
         </View>
+        <Input keyboardType="email-address"></Input>
         <View style={styles.titulo}>
           <Ionicons name="call" size={30} color="#3A4D6A" />
           <Text style={styles.tituloText}> Contato </Text>
         </View>
-
+        <Input keyboardType="phone-pad"></Input>
         <TouchableOpacity
           style={[
             styles.buttonCancelar,
@@ -45,7 +58,8 @@ const Perfil = () => {
               shadowOpacity: 1,
               shadowRadius: 100,
               elevation: 10,
-              marginTop: 150,
+              marginTop: 50,
+              paddingBottom: 10,
             },
           ]}
         >
@@ -54,15 +68,36 @@ const Perfil = () => {
           </Text>
         </TouchableOpacity>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.buttonCancelar}>
+          <TouchableOpacity style={styles.buttonCancelar} onPress={fechar}>
             <Text style={styles.buttonText}>Cancelar</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.buttonSalvar}>
+          <TouchableOpacity style={styles.buttonSalvar} onPress={() => setModalVisible(true)}>
             <Text style={styles.buttonText}>Salvar</Text>
           </TouchableOpacity>
         </View>
+
+        <Modal
+        animationType="slide" 
+        transparent={true}    
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)} 
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Configuração salva com sucesso</Text>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={fechar}
+            >
+              <Text style={styles.textStyle}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
       </CardElevado>
     </View>
+    </ScrollView>
+    
   );
 };
 
@@ -76,7 +111,7 @@ const styles = StyleSheet.create({
   titulo: {
     flexDirection: "row",
     alignItems: "center",
-    paddingTop: 50,
+    paddingTop: 40,
   },
   tituloText: {
     fontSize: 20,
@@ -87,6 +122,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     marginTop: "auto",
+    paddingTop: 20,
   },
   buttonCancelar: {
     backgroundColor: "red",
@@ -106,6 +142,33 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalView: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  closeButton: {
+    backgroundColor: "red",
+    padding: 10,
+    borderRadius: 10,
   },
 });
 
